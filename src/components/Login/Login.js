9aed41fd8336a1157483bf35f5e5aea1f5c93b6c
Kev <ngynkvn@gmail.com>
2017-10-14 04:10:09 -0400

    Routing, working on information gathering scene where user inputs current pantry.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
+import {Actions} from 'react-native-router-flux';
+import {RkButton} from 'react-native-ui-kitten';
+
+
 
 export default class Login extends Component {
     render() {
         return (
             <View style={styles.container}>
 
+            <RkButton onPress={() => Actions.infoGather()}> Next </RkButton>
             </View>
         );
     }
