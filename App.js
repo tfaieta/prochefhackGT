e8781e2c75fab71dd7e9017a76171cd3a7426f5b
tfaieta <tfaieta@gmail.com>
2017-10-14 02:26:57 -0400

    Here you go again

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import login from './src/components/Login/login'
+import Login from './src/components/Login/Login';
 
 export default class App extends React.Component {
   render() {
     return (
-        <login />
+        <Login />
     );
   }
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 
-export default class login extends Component {
+export default class Login extends Component {
     render() {
         return (
             <View style={styles.container}>
