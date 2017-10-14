5d90d357bd1ef4dcefd17314b5681a1c19cf7dd8
Kev <ngynkvn@gmail.com>
2017-10-14 19:36:19 -0400

    stuff

--- src/components/Info/InfoGather.js
+++ src/components/Info/InfoGather.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {StyleSheet, Text, View, FlatList, Button, TouchableOpacity} from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    Button,
+    TouchableOpacity
+} from 'react-native';
 import processData from './process'
 import {Actions} from 'react-native-router-flux';
 import {RkButton, RkTextInput} from 'react-native-ui-kitten';
@@ -14,30 +21,60 @@ export default class InfoGather extends React.Component {
             ingredients: []
         };
         this.dict = processData(data);
-    }
+    };
     render() {
         return (
             <View style={styles.break}>
                 <Text style={styles.center}>What's in your pantry?</Text>
                 <RkTextInput
+                    autocomplete={false}
                     autoFocus={true}
+                    ref={i => this.userInput = i}
                     placeholder="Enter your ingredients"
-                    onChangeText={(text) => this.setState({text})}/> 
-                    {this.state.text.length < 2
+                    onChangeText={(text) => this.setState({text})}/>{this.state.text.length < 2
                     ? null
                     : <FlatList
                         data={this.getData()}
-                        keyExtractor = {(item, i) => item }
+                        keyExtractor=
+                        {(item, i) => item }
                         renderItem=
-                        { ({item}) => <TouchableOpacity  onPress={() => this.handlePress(item)} ><Text style={styles.item}>{item}</Text></TouchableOpacity>}/>}
-                {/* <FlatList data={this.showIngredients(this.ingredients)} onPress={this.handleSelect}/> */}
+                        { ({item}) => <TouchableOpacity onPress={(e) => this.handlePress(e,item)} ><Text style={styles.item}>{item}</Text></TouchableOpacity>}/>}
+                <View style={{
+                    paddingTop: 50
+                }}>
+                    <Text>Selection:</Text>
+                </View>
+                {this.state.ingredients.length > 0
+                    ? <FlatList
+                            data={this.state.ingredients}
+                            keyExtractor=
+                            {(item, i) => item}
+                            renderItem=
+                            { ({item}) => <TouchableOpacity onPress={()=>this.deselect(item)}><Text style={styles.item}>{item}</Text></TouchableOpacity>}/>
+                    : null}
             </View>
-        );
+        )
     };
-    handlePress(e)
+
+    handlePress(e,item)
     {
-        console.log(e);
+        this.setState({
+            ingredients: this
+                .state
+                .ingredients
+                .concat([item])
+        ,text:''});
+        // this.refs.userInput.value = '';
     };
+    deselect(e)
+    {
+        this.setState({
+            ingredients: this
+                .state
+                .ingredients
+                .filter(i => e != i)
+        });
+    }
     getData() {
         var key = this
             .state
@@ -50,7 +87,9 @@ export default class InfoGather extends React.Component {
 
         if (results != null) {
             for (var i = 0; i < results.length; i++) {
-                arr.push(results[i]);
+                if (this.state.ingredients.indexOf(results[i]) == -1) {
+                    arr.push(results[i]);
+                }
             }
         }
         return arr;
